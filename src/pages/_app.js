b80a58da1c52e19ecaf2ee79c39b38c4ee1243b8
AlanPan0131 <panalan0131@gmail.com>
2023-06-20 17:02:02 +0800

    update

--- src/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import { NextUIProvider } from '@nextui-org/react';
-
-export default function MyApp({ Component, pageProps }) {
-    return (
-        <NextUIProvider>
-            <Component {...pageProps} />
-        </NextUIProvider>
-    )
-}
